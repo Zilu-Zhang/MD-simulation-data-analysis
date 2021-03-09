3bb91ed5dec05658efae3b089cece6b721cfbd50
Zilu Zhang <75759484+Zilu-Zhang@users.noreply.github.com>
2021-03-09 10:55:11 -0500

    Update Hbond.py

--- Hbond.py
+++ Hbond.py
@@ -1,41 +1,47 @@
+import os
+import os.path
 import mdtraj as md
-import sys
-
-filepath = './openmm_sorafenib_glycyrrhizin.pdb'
+import numpy as np
+import pandas as pd
+import openpyxl as pxl
 
 def label(hbond):
-    r1 = traj.topology.atom(hbond[0])
-    r2 = traj.topology.atom(hbond[2])
-    if ('ZPE' in str(r1)) is not ('ZPE' in str(r2)):
-        n = 1
-    else:
-        n = 0
-    return (n, '%s -- %s' % (r1, r2))
-
-traj = md.load_frame(filepath, 0)
-hbonds = md.baker_hubbard(traj)
-number = 0
-readout = open('readout.txt', 'a')
-sys.stdout = readout
-for hbond in hbonds:
-    n, note = label(hbond)
-    number += n
-    print(note)
-print('total Hbonds: ', len(hbonds))
-print('drug-excipient Hbonds: ', number)
-print('--------------------------------')
-readout.close()
-
-traj = md.load_frame(filepath, 199)
-hbonds = md.baker_hubbard(traj)
-number = 0
-readout = open('readout.txt', 'a')
-sys.stdout = readout
-for hbond in hbonds:
-    n, note = label(hbond)
-    number += n
-    print(note)
-print('total Hbonds: ', len(hbonds))
-print('drug-excipient Hbonds: ', number)
-print('--------------------------------')
-readout.close()
+            r1 = traj.topology.atom(hbond[0])
+            r2 = traj.topology.atom(hbond[2])
+            n = 0
+            if ('ZPE' in str(r1)) is not ('ZPE' in str(r2)):
+                n = 1
+			return n
+		
+n_frames = 200
+for filename in os.listdir('./'):
+	if filename.endswith('.pdb'):
+		excipient_name = filename[17:-4]
+		total_Hbonds = np.zeros(n_frames - 1)
+		interactive_Hbonds = np.zeros(n_frames - 1)
+		ratio = np.zeros(n_frames - 1)
+		
+		for i in range(n_frames):
+			traj = md.load_frame(filename, i)
+			hbonds = md.baker_hubbard(traj)
+			number = 0
+			for hbond in hbonds:
+				n = label(hbond)
+				number += n
+			
+			total_Hbonds[i] = len(hbonds)
+			interactive_Hbonds[i] = number
+			ratio[i] = interactive_Hbonds[i] / total_Hbonds[i]
+		
+		df = pd.DataFrame({'total_Hbonds': total_Hbonds, 'interactive_Hbonds': interactive_Hbonds, 'ratio': ratio})
+		
+		if not os.path.isfile('Hbonds.xlsx'):
+			df.to_excel('Hbonds.xlsx', '%s' % excipient_name, index = True)
+			
+		else:
+			excel_book = pxl.load_workbook('Hbonds.xlsx')
+			with pd.ExcelWriter('Hbonds.xlsx', engine = 'openpyxl') as writer:
+				writer.book = excel_book
+				writer.sheets = {worksheet.title: worksheet for worksheet in excel_book.worksheets}
+				df.to_excel(writer, '%s' % excipient_name, index = True)
+				writer.save()
