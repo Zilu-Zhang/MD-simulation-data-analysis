33d1ad3808abfa163c7022e85ab5c3fd1f7277bf
Zilu Zhang <75759484+Zilu-Zhang@users.noreply.github.com>
2021-04-19 18:58:23 -0400

    Update surface_area.py

--- SASA/surface_area.py
+++ SASA/surface_area.py
@@ -4,32 +4,33 @@ import openpyxl as pxl
 import os
 import os.path
 
-filename = 'openmm_sorafenib_ursodiol.pdb'
-excipient_name = filename[17:-4]
-lst = []
-n_frame = 200
-traj = md.load(filename)
-surface = md.shrake_rupley(traj, mode = 'residue')
-top = traj.topology
-n_residue = 16
-
-for i in range(n_frame):
-    area = {}
-    for j in range(n_residue):
-        residues = top.residue(j)
-        area[residues] = surface[i][j]
-    lst.append(area)
-
-df = pd.DataFrame(lst)
-df.loc['total'] = df.sum()
-
-if not os.path.isfile('sasa.xlsx'):
-    df.to_excel('sasa.xlsx', '%s' % excipient_name, index = True)
-
-else:
-    excel_book = pxl.load_workbook('sasa.xlsx')
-    with pd.ExcelWriter('sasa.xlsx', engine = 'openpyxl') as writer:
-        writer.book = excel_book
-        writer.sheets = {worksheet.title: worksheet for worksheet in excel_book.worksheets}
-        df.to_excel(writer, '%s' % excipient_name, index = True)
-        writer.save()
+n_frames = 200
+for filename in os.listdir('./'):
+    if filename.endswith('.pdb'):
+        excipient_name = filename[17:-4]
+        lst = []
+        traj = md.load(filename)
+        surface = md.shrake_rupley(traj, mode = 'residue')
+        top = traj.topology
+        n_residue = 16
+        
+        for i in range(n_frame):
+            area = {}
+            for j in range(n_residue):
+                residues = top.residue(j)
+                area[residues] = surface[i][j]
+            lst.append(area)
+            
+        df = pd.DataFrame(lst)
+        df.loc['total'] = df.sum()
+        
+        if not os.path.isfile('sasa.xlsx'):
+            df.to_excel('sasa.xlsx', '%s' % excipient_name, index = True)
+        
+        else:
+            excel_book = pxl.load_workbook('sasa.xlsx')
+            with pd.ExcelWriter('sasa.xlsx', engine = 'openpyxl') as writer:
+                writer.book = excel_book
+                writer.sheets = {worksheet.title: worksheet for worksheet in excel_book.worksheets}
+                df.to_excel(writer, '%s' % excipient_name, index = True)
+                writer.save()
