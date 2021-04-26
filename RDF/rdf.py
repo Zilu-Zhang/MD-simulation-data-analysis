fc9a1007c238e55108ce2c573fe8361ba24aea0e
Zilu Zhang <75759484+Zilu-Zhang@users.noreply.github.com>
2021-04-26 15:59:08 -0400

    Update rdf.py

--- RDF/rdf.py
+++ RDF/rdf.py
@@ -20,18 +20,34 @@ for filename in os.listdir('./'):
         excipient_name = filename[17:-4]
         traj = md.load(filename)
         top = traj.topology
+        ori = 0
+        total = np.zeros(3200)
         
         for i in range(n_frames):
-          start = 0
-          position = np.zeros((17,3))
-          for j in range(16):
-            res = top.residue(j)
-            length = res.n_atoms
-            start += length
-            x = mean(traj.xyz[i, start:start + length - 1, 0])
-            y = mean(traj.xyz[i, start:start + length - 1, 1])
-            z = mean(traj.xyz[i, start:start + length - 1, 2])
-            position[j][:] = x, y, z
-          position[-1][:] = mean(array[:-1][0]), mean(array[:-1][1]), mean(array[:-1][2])
+            start = 0
+            position = np.zeros((17,3))
+            for j in range(16):
+                res = top.residue(j)
+                length = res.n_atoms
+                start += length
+                x = mean(traj.xyz[i, start:start + length - 1, 0])
+                y = mean(traj.xyz[i, start:start + length - 1, 1])
+                z = mean(traj.xyz[i, start:start + length - 1, 2])
+                position[j][:] = x, y, z
+            position[-1][:] = mean(array[:-1][0]), mean(array[:-1][1]), mean(array[:-1][2])
+            
+            distance = np.zeros(16)
+            for i in range(16):
+                distance[i] = dis(position[-1], position[i])
+            
+            total[ori:ori + 16] = distance
+            ori += 17
+            
+            r_range = np.array([0.0, 2.5])
+            bin_width = 0.005
+            n_bins = int((r_range[1] - r_range[0]) / bin_width)
+            g_r, edges = np.histogram(total, range=r_range, bins=n_bins)
+            g_r = g_r / 3200
+            r = 0.5 * (edges[1:] + edges[:-1])
 
           
