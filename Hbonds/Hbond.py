e1d4d6d8a9a40cf3701539a8e0b2c8f0a3a8d13c
Zilu Zhang <75759484+Zilu-Zhang@users.noreply.github.com>
2021-06-04 10:36:51 -0400

    Update Hbond.py

--- Hbonds/Hbond.py
+++ Hbonds/Hbond.py
@@ -1,4 +1,9 @@
-### This will generate a Hbonds.xlsx file containing information about total Hbonds, interactive Hbonds and their ratios of each excipient
+'''
+
+This will generate a Hbonds.xlsx file containing information about total Hbonds, interactive Hbonds and their ratios of each excipient
+
+'''
+# import packages
 import os
 import os.path
 import mdtraj as md
@@ -6,32 +11,30 @@ import numpy as np
 import pandas as pd
 import openpyxl as pxl
 
+# recognize the number of interactive H bonds between drug and excipient molecules
 def label(hbond):
     r1 = traj.topology.atom(hbond[0])
     r2 = traj.topology.atom(hbond[2])
-    n = 0
-    if str(r1)[:3] != str(r2)[:3]:
-        n = 1
+    n = 1 if str(r1)[:3] != str(r2)[:3] else 0
     return n
 
-n_frames = 200
-for filename in os.listdir('./'):
+for filename in os.listdir('./'): 
     if filename.endswith('.pdb'):
+	full = md.load(filename)
+	n_frames = len(full)
         excipient_name = filename[17:-4]
-        total_Hbonds = np.zeros(n_frames)
-        interactive_Hbonds = np.zeros(n_frames)
-        ratio = np.zeros(n_frames)
+        total_Hbonds, interactive_Hbonds, ratio = np.zeros(n_frames), np.zeros(n_frames), np.zeros(n_frames)
 
         for i in range(n_frames):
-            traj = md.load_frame(filename, i)
+            traj = full[i]
             hbonds = md.baker_hubbard(traj)
             number = 0
             for hbond in hbonds:
                 n = label(hbond)
                 number += n
-                total_Hbonds[i] = len(hbonds)
-                interactive_Hbonds[i] = number
-                ratio[i] = interactive_Hbonds[i] / total_Hbonds[i]
+            total_Hbonds[i] = len(hbonds)
+            interactive_Hbonds[i] = number
+            ratio[i] = interactive_Hbonds[i] / total_Hbonds[i]
 
         df = pd.DataFrame({'total_Hbonds': total_Hbonds, 'interactive_Hbonds': interactive_Hbonds, 'ratio': ratio})
 		
@@ -45,3 +48,4 @@ for filename in os.listdir('./'):
                 writer.sheets = {worksheet.title: worksheet for worksheet in excel_book.worksheets}
                 df.to_excel(writer, '%s' % excipient_name, index = True)
                 writer.save()
+		writer.close()
