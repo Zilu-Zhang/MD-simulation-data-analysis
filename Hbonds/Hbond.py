99b2cfee93a045ad005c776d0b98b96d5ab7b14a
Zilu Zhang <75759484+Zilu-Zhang@users.noreply.github.com>
2021-06-07 14:48:28 -0400

    Update Hbond.py

--- Hbonds/Hbond.py
+++ Hbonds/Hbond.py
@@ -13,38 +13,59 @@ from statistics import mean
 
 # recognize the interactive H bonds between drug and excipient molecules
 def label(hbond):
-    r1 = traj.topology.atom(hbond[0])
-    r2 = traj.topology.atom(hbond[2])
-    n = 1 if str(r1)[:3] != str(r2)[:3] else 0
-    return n
+    r1 = str(traj.topology.atom(hbond[0]))
+    r2 = str(traj.topology.atom(hbond[2]))
+    n = 1 if r1[:3] != r2[:3] else 0
+    if n == 1:
+	m = 1 if r1[:3] == drug_code else 0
+    return n, m
 
 # main code
 for filename in os.listdir('./'): 
     if filename.endswith('.pdb'):
 	full = md.load(filename)
 	n_frames = len(full)
+	drug_code = str(full.topology.residue(0))
         excipient_name = filename[17:-4]
-        total_Hbonds, interactive_Hbonds, ratio = np.zeros(n_frames + 1), np.zeros(n_frames + 1), np.zeros(n_frames + 1)
-
+        total_Hbonds, interactive_Hbonds, drug_donor, interactive_ratio = np.zeros(n_frames + 1), np.zeros(n_frames + 1), np.zeros(n_frames + 1), np.zeros(n_frames + 1)
+	frame_index = np.zeros(n_frames + 1)
+	
         for i in range(n_frames):
             traj = full[i]
             hbonds = md.baker_hubbard(traj)
             number = 0
+            donor = 0
             for hbond in hbonds:
-                n = label(hbond)
+                n, m = label(hbond)
                 number += n
+		donor += m
+		
+		
+            frame_index[i] = i
             total_Hbonds[i] = len(hbonds)
             interactive_Hbonds[i] = number
-            ratio[i] = interactive_Hbonds[i] / total_Hbonds[i]
+            interactive_ratio[i] = interactive_Hbonds[i] / total_Hbonds[i]
+            drug_donor[i] = donor
+            drug_donor_ratio[i] = drug_donor[i] / interactive_Hbonds[i]
+
+
 	
+	frame_index[-1] = 'summary' # row title
 	total_Hbonds[-1] = sum(total_Hbonds[:-2]) # total number of all Hbonds
-	interactive_Hbonds[-1] = sum(total_Hbonds[:-2]) # total number of all interactive Hbonds
-	ratio[-1] = mean(ratio[:-2]) # mean value of interactive Hbonds ratios
+	interactive_Hbonds[-1] = sum(interactive_Hbonds[:-2]) # total number of all interactive Hbonds
+	interactive_ratio[-1] = mean(interactive_ratio[:-2]) # mean value of interactive Hbonds ratios
+	drug_donor[-1] = sum(drug_donor[:-2]) # total number of all interactive Hbonds
+	drug_donor_ratio[-1] = mean(drug_donor_ratio[:-2]) # mean value of interactive Hbonds ratios
 	
-        df = pd.DataFrame({'total_Hbonds': total_Hbonds, 'interactive_Hbonds': interactive_Hbonds, 'ratio': ratio})
+        df = pd.DataFrame({'frame_index': frame_index, 
+			   'total_Hbonds': total_Hbonds,
+			   'interactive_Hbonds': interactive_Hbonds, 
+			   'interactive_ratio':interactive_ratio,
+			   'drug_donor':drug_donor,
+			   'drug_donor_ratio': drug_donor_ratio})
 		
         if not os.path.isfile('Hbonds.xlsx'):
-            df.to_excel('Hbonds.xlsx', '%s' % excipient_name, index = True)
+            df.to_excel('Hbonds.xlsx', '%s' % excipient_name)
 
         else:
             excel_book = pxl.load_workbook('Hbonds.xlsx')
